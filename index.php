a39e79a95f5b4b54179e84b146dfeda6a58ea6d9
Douglas Naphas <dnaphas@sevone.com>
2014-09-08 19:17:25 -0400

    Isolating the presentation, README

--- index.php
+++ index.php
@@ -8,27 +8,13 @@
 
 $mysqli = new mysqli( 'localhost', 'symf', 'symfony_pw', 'blog_db' );
 $result = $mysqli->query( "SELECT id, title FROM post" );
-?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>List of Posts</title>
-	</head>
-	<body>
-		<h1>List of Posts</h1>
-		<ul>
-			<?php while ( $row = $result->fetch_assoc() ): ?>
-			<li>
-				<a href = "/show.php?id=<?php echo $row['id'] ?>">
-					<?php echo $row['title'] ?>
-				</a>
-			</li>
-			<?php endwhile; ?>
-		</ul>
-	</body>
-</html>
+$posts = array();
+while( $row = $result->fetch_assoc() ){
+	$posts[] = $row;
+}
 
-<?php
 $mysqli->close();
-?>
+
+// Include the HTML presentation code.
+require 'templates/list.php';
