a4a334f85b37c36192da008518285f4df7396b13
Douglas Naphas <dnaphas@sevone.com>
2014-09-10 08:25:54 -0400

    model.php stub

---
+++ model.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dnaphas
+ * Date: 9/9/14
+ * Time: 10:03 PM
+ */
+
+function open_database_connection(){
+
+}
+
+function close_database_connection( $link ){
+
+}
+
+function get_all_posts(){
+
+}
