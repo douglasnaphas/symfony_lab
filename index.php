f8e0b4a62cf13dcf9b2e93b601dbe6e1fd70e919
Douglas Naphas <dnaphas@sevone.com>
2014-09-13 18:23:23 -0400

    Front controller stubs

--- index.php
+++ index.php
@@ -6,8 +6,15 @@
  * Time: 12:16 PM
  */
 
+// load and initialize any global libraries
 require_once 'model.php';
+require_once 'controllers.php'
 
+// route the request internally
+$uri = $_SERVER['REQUEST_URI'];
+if('index.php' == $uri){
+	list_action();
+}
 $posts = get_all_posts();
 
 // Include the HTML presentation code.
