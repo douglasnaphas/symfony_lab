aab1f43ae1f722fda0e1b335c76c8dd230a8ab97
Douglas Naphas <dnaphas@sevone.com>
2014-10-31 19:23:39 -0400

    Slightly more BlogController

--- src/Acme/BlogBundle/Controller/BlogController.php
+++ src/Acme/BlogBundle/Controller/BlogController.php
@@ -9,37 +9,15 @@ namespace Acme\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class
+class BlogController extends Controller
+{
+	public function listAction()
+	{
+		$posts = $this->get('doctrine')
+			->getManager()
+			->createQuery('SELECT p FROM AcmeBlogBundle:Post p')
+			->execute();
+	}
 
 
-
-use Symfony\Component\HttpFoundation\Response;
-
-function list_action(){
-	$posts = get_all_posts();
-	$html = render_template('templates/list.php', array('posts' => $posts));
-
-	return new Response($html);
-}
-
-function show_action($id){
-	$post = get_post_by_id($id);
-	$html = render_template('templates/show.php', array('post' => $post));
-
-	return new Response($html);
-}
-
-/**
- * helper function to render templates
- * @param string $path
- * @param array $args
- * @return string HTML template
- */
-function render_template($path, array $args){
-	extract($args);
-	ob_start();
-	require $path;
-	$html = ob_get_clean();
-
-	return $html;
 }
